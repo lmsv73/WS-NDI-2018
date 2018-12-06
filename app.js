c8b8480d1faa3a5637dce5e19f2debed9a327b66
Alexandre Muhl <alexandre@netdev.fr>
2018-12-06 23:06:49 +0100

    cors

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var app = express();
 
 var meteoRoute = require('./routes/meteo');
 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

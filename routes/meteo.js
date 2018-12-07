e239d7a58bc286e94125ce2b108b2916fb89210c
Alexandre Muhl <alexandre@netdev.fr>
2018-12-07 07:00:06 +0100

    Correctif webhook

--- routes/meteo.js
+++ routes/meteo.js
@@ -39,7 +39,7 @@ function tempwebhook(req, res, next){
         db.collection('temp').find({}).sort({date: -1}).limit(1).toArray((err, result) => {
             client.close();
             res.json({
-                "fulfillmentText": ""+result.temp
+                "fulfillmentText": ""+result[0].temp
             });
         });
     })
